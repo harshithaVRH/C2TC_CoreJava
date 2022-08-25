e29492235cf6cce333021f97425e8f8fd56ce570
HarsHithu <HarsHithu@DESKTOP-86D5QV6>
2022-08-25 21:52:00 +0530

    commit

--- JavaDemos/src/com/tnsif/lesson3g/SmartCode.java
+++ JavaDemos/src/com/tnsif/lesson3g/SmartCode.java
@@ -1,26 +1,39 @@
+// program to demostrate usage of source command in eclipse
 package com.tnsif.lesson3g;
 
-class Car1 {
-	private String carName ="Seltos";
-	private Integer topSpeed = 100;
+class CarOne {
+	private String carName ;
+	private Integer topSpeed;
 	
-	public Car1(String carName, Integer topSpeed) {
-		super();
+	public CarOne(String carName, Integer topSpeed) {
 		this.carName = carName;
 		this.topSpeed = topSpeed;
+		System.out.println("Car name is "+carName);
+		System.out.println("Top speed is "+topSpeed);
 	}
 
-	void displayOutput()
-	{
-		System.out.println("Car ");
-		
+	public String getCarName() {
+		return carName;
 	}
+
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+
+	public Integer getTopSpeed() {
+		return topSpeed;
+	}
+
+	public void setTopSpeed(Integer topSpeed) {
+		this.topSpeed = topSpeed;
+	}	
+	
 }
 public class SmartCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		CarOne obj = new CarOne("Seltos",100);
 	}
 
 }
